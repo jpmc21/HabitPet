f3bc667b4bd2d22f6f14042ebaa54a393a60e032
oonamo <onam75hernandez@gmail.com>
2026-05-23 11:09:10 -0700

    update checkenv to show how to generate secret

--- scripts/checkenv.js
+++ scripts/checkenv.js
@@ -4,6 +4,9 @@ const requiredEnvVars = ["MONGO_URI", "JWT_SECRET", "PORT"]
 
 for (const varName of requiredEnvVars) {
   if (!process.env[varName]) {
+    if (varName === "JWT_SECRET") {
+        console.log('use the following to generate JWT_SECRET:\nnode -e "console.log(require(\'crypto\').randomBytes(64).toString(\'hex\'))"');
+    }
     console.error(`Error: Missing required environment variable ${varName}`);
     process.exit(1);
   }
