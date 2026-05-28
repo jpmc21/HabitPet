decc4270bbb9a20a5c01ba385a39c2907f18f045
Jacob Garcia <jgarcia042405@ucla.edu>
2026-05-28 16:54:24 -0700

    Fixed ghost logout bug and added backend verify route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import axios from 'axios'
 import PetCard from './components/PetCard'
 import Habits from './components/Habits'
 import Login from './components/Login'
 import Register from './components/Register'
+import { useState, useEffect } from 'react'
+
 import './App.css'
 
 function App() {
@@ -12,6 +14,32 @@ function App() {
 
   // tracks which page to show when not logged in
   const [page, setPage] = useState('login')
+
+  const [isvalidating, setIsValidating] = useState(true)
+
+useEffect(() => {
+async function verifyToken() {
+  if (!token) {
+    setIsValidating(false)
+    return
+  }
+try {
+  await axios.get('http://localhost:5000/api/auth/verify', {
+    headers: {Authorization: `Bearer ${token}`}
+  })
+  setIsValidating(false)
+
+
+} catch (error) {
+  console.error("Token is invalid or expired. Logging out...")
+  localStorage.removeItem('token')
+  setToken(null)
+  setIsValidating(false)
+}
+}
+verifyToken()
+}, [token])
+
 
   function handleLogin(newToken) {
     // login worked, save token
@@ -29,6 +57,16 @@ function App() {
     setToken(null)
     setPage('login')
   }
+
+  if (isvalidating) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Loading Habit...</h2>
+      </header>
+    </div>
+  )
+}
 
   // not logged in yet, show login or register
   if (!token) {
