d060351570a523d213066012c5e0ac38525c8fe4
oonamo <onam75hernandez@gmail.com>
2026-06-01 20:07:10 -0700

    add check to ensure PORT != SERVER_PORT

--- scripts/checkenv.js
+++ scripts/checkenv.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 
 const requiredEnvVars = ["MONGO_URI", "JWT_SECRET", "SERVER_PORT"]
 
+if (process.env.PORT && process.env.PORT === process.env.SERVER_PORT) {
+  console.error(`Error: PORT environment variable (${process.env.PORT}) should not match SERVER_PORT (${process.env.SERVER_PORT}).`);
+  process.exit(1);
+}
+
 for (const varName of requiredEnvVars) {
   if (!process.env[varName]) {
     if (varName === "JWT_SECRET") {
