04baf47a6b49c0b9f8dee5fbc52cf5dade0241c0
Jacob Garcia <jgarcia042405@ucla.edu>
2026-06-03 14:34:04 -0700

    fixed the way the exp works with leveling up, now for level 0 we have 0/50 exp needed, then after that you will need 0/100 then it will go to 0/150.

--- server/routes/habits.js
+++ server/routes/habits.js
@@ -213,7 +213,7 @@ router.post("/:id/complete", async (req, res) => {
     user.points += habit.reward;
 
     // pet gets exp when u complete a habit
-    user.pet.exp += 15
+    user.pet.exp += 15;
 
     await user.save();
     await habit.save();
@@ -272,8 +272,8 @@ router.post("/:id/undo", async (req, res) => {
     habit.exp = Math.max(0, habit.exp - habit.reward);
     user.points = Math.max(0, user.points - habit.reward);
     // take away exp if u undo the habit
-    user.pet.exp = Math.max(0, user.pet.exp - 15)
-
+user.pet.exp = Math.max(0, user.pet.exp - 15);
+if (user.pet.exp < 0) user.pet.exp = 0;
     await habit.save();
     await user.save();
 
